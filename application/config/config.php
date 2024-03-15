47bf302a46d3dbb6c4ccf28a2c9c0d78d6f2c787
tech-girish <girish.extravis@gmail.com>
2024-03-15 12:07:43 +0530

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "https");
 $base_url .= "://".$_SERVER['HTTP_HOST'];
 $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base_url;
