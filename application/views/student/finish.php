974f70a8d00858d92fae82696a19c469b4511f92
kavya <kavya@medhatech.in>
2024-03-25 15:29:54 +0530

    change

---
+++ application/views/student/finish.php
@@ -0,0 +1,37 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <!-- <div class="col-sm-6">
+            <h4>Education Details From</h4>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Education Details</li>
+            </ol>
+          </div> -->
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <div class="card shadow mb-4 m-4 p-2">
+          <div class="card-body">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="form-label custom-control-label custom-cb" for="customCheck1"> By clicking Agree and Continue </label>
+            </div>
+            <p class="m-0">I hereby declare that the above information is true and correct to the best of my knowledge and belief.</p>
+            <p class="m-0">I promise to abide by the Regulations and Orders of the College and University.</p>
+            <p class="m-0">I hereby agree to undergo the N.C.C. & N.S.S. Training/Physical Culture Training prescribed by the college during the period of my study in the College.</p>
+            <p class="m-0">I promise that I don’t indulge in ragging and abide by the rules of conduct and discipline of the institution.</p>
+            <p class="m-0">I undertake to attend not less than 75% of the working periods in each of the subjects of the course.</p>
+            <p class="m-0">I shall not be eligible to appear for the exam if I fall short of attendance beyond the condonable limit.</p>
+            <p class="m-0">I agree to pay prescribed fees in time and do not claim refund of any fees paid by me to the institution.</p>
+            <center><a href="https://bmscw.edu.in/admissions/student/app_submit" class="btn btn-success btn-user text-center text-bold px-5 m-2" name="app_submit" id="app_submit">SUBMIT APPLICATION</a></center>
+          </div>
+        </div>
+  </div>
+  <!-- /.content-wrapper -->
