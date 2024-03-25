60eacb9d0e68a5bd7ab981910e13f5f86e884859
Sreeni <seenu619@gmail.com>
2024-03-25 15:29:39 +0530

    departments

--- application/views/admin/template/admin_header.php
+++ application/views/admin/template/admin_header.php
@@ -7,26 +7,32 @@
     <title><?= $page_title; ?> | MCE Campus ERP Portal</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
     <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/jqvmap/jqvmap.min.css"> -->
 
 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/adminlte.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet"
+        href="<?php echo base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
@@ -77,11 +83,13 @@
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/dashboard" class="nav-link <?= $menu_active = ($menu == "dashboard") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/dashboard"
+                                class="nav-link <?= $menu_active = ($menu == "dashboard") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard
@@ -125,7 +133,8 @@
                             </ul>
                         </li> -->
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/Enquiries" class="nav-link <?= $menu_active = ($menu == "enquiries") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/Enquiries"
+                                class="nav-link <?= $menu_active = ($menu == "enquiries") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-th"></i>
                                 <p>
                                     All Enquiries
@@ -134,7 +143,8 @@
                         </li>
 
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/newEnquiry" class="nav-link <?= $menu_active = ($menu == "newEnquiry") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/newEnquiry"
+                                class="nav-link <?= $menu_active = ($menu == "newEnquiry") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-plus"></i>
                                 <p>
                                     New Enquiry
@@ -151,7 +161,7 @@
                                     <i class="right fas fa-angle-left"></i>
                                 </p>
                             </a>
-                            <ul class="nav nav-treeview" >
+                            <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="<?php echo base_url(); ?>admin/admissions" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
@@ -200,12 +210,22 @@
                                         <p>Archieved</p>
                                     </a>
                                 </li>
-                              
+
                             </ul>
                         </li>
                         <li class="nav-header">SETTINGS</li>
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/feestructure" class="nav-link <?= $menu_active = ($menu == "feestructure") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/departments"
+                                class="nav-link <?= $menu_active = ($menu == "departments") ? 'active' : ''; ?>">
+                                <i class="nav-icon fas fa-bars"></i>
+                                <p>
+                                    Departments
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="<?php echo base_url(); ?>admin/feestructure"
+                                class="nav-link <?= $menu_active = ($menu == "feestructure") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-bars"></i>
                                 <p>
                                     Fee Structure
@@ -214,13 +234,15 @@
                             </a>
                         </li>
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/reports" class="nav-link <?= $menu_active = ($menu == "reports") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/reports"
+                                class="nav-link <?= $menu_active = ($menu == "reports") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-chart-pie"></i>
                                 <p>Reports</p>
                             </a>
                         </li>
                         <li class="nav-item ">
-                            <a href="<?php echo base_url(); ?>admin/feestructure" class="nav-link <?= $menu_active = ($menu == "changePassword") ? 'active' : ''; ?>">
+                            <a href="<?php echo base_url(); ?>admin/feestructure"
+                                class="nav-link <?= $menu_active = ($menu == "changePassword") ? 'active' : ''; ?>">
                                 <i class="nav-icon fas fa-fingerprint"></i>
                                 <p>
                                     Change Password
