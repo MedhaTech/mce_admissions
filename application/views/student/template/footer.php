4adaebeed87bba4519c7daed565101f8311bbc3b
Sreeni <seenu619@gmail.com>
2024-03-28 22:40:08 +0530

    fix

--- application/views/student/template/footer.php
+++ application/views/student/template/footer.php
@@ -1,23 +1,18 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="">MedhaTech</a>.</strong>
+    Copyright &copy; 2024 <a href="">MCE</a>.
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+        Designed and Developed by <a href="medhatech.in" target="_blank">MedhaTech</b>
     </div>
-  </footer>
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+$.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -34,7 +29,8 @@
 <script src="<?php echo base_url();?>assets/plugins/moment/moment.min.js"></script>
 <script src="<?php echo base_url();?>assets/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?php echo base_url();?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php echo base_url();?>assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+</script>
 <!-- Summernote -->
 <script src="<?php echo base_url();?>assets/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
@@ -46,4 +42,5 @@
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="<?php echo base_url();?>assets/js/pages/dashboard.js"></script>
 </body>
+
 </html>
