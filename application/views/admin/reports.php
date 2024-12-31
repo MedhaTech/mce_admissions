05ef32e065c2f4688cdac0c1b388bb79170897e2
kavya <kavya@medhatech.in>
2024-12-31 18:12:23 +0530

    changes

--- application/views/admin/reports.php
+++ application/views/admin/reports.php
@@ -73,7 +73,7 @@
                                       <li><?php echo anchor('admin/category_admissions_report', 'Category wise Admissions Report'); ?> </li>
                                       <li><?php echo anchor('admin/admissionsyearbook', 'Admission Year Book'); ?> </li>
                                       <!-- <li><?php echo anchor('admin/consolidated_report', 'Consolidated 2024-25 1st year & 3rd semester student list'); ?> </li> -->
-                                      <li><?php echo anchor('admin/consolidated_report/1', 'Consolidated 2024-25 1st year & 3rd semester student list'); ?></li>
+                                      <li><?php echo anchor('admin/consolidated_report/1', 'Consolidated B.E. Students List'); ?></li>
 
 
                                   </ul>
